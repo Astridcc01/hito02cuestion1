112e8d6fe2ba14607df5ed6adfa0094aac90455b
Astridcc01 <93380210+Astridcc01@users.noreply.github.com>
2021-12-10 14:33:18 +0100

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package com.cuestion1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ficha de Trabajador");
+		Trabajador trabajador1=new Trabajador("Juan","Madrid",1500f,true);
+		System.out.println(trabajador1.toString());
+	}
+
+}
